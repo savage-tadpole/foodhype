e8fdfee754acb39e223dd79b82dbecb5fdf7ae09
Emily Shenfield <emily.shenfield@gmail.com>
2015-06-11 16:09:19 -0700

    refactor: removed unneeded console.logs and comments

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -17,7 +17,6 @@ var AppView = React.createClass({
     this.render();
   },
   getInitialState: function() {
-    // Fakey data
     return {
       selectedMarkerData: {
         display: false,
@@ -82,10 +81,8 @@ var WindowView = React.createClass({
   },
   render: function() {
     if(this.props.data.display === false) {return(<div></div>);}
-    console.log(this.props.data);
     var instagramPictureUrl = this.props.data.instagramPictureUrl ||
       'http://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg';
-    console.log(instagramPictureUrl);
     return (
       <div id="window">
         <div id="windowTitle">{this.props.data.name}</div>
@@ -101,21 +98,6 @@ var WindowView = React.createClass({
     )
   }
 });
-
-// var LinkButton = React.createClass({
-//   render: function() {
-//     if(!this.props.url) {
-//       return (<span></span>)
-//     } else {
-//       return(
-//         <a href={this.props.url}><button className="linkButton" id={this.props.id}></button></a>
-//       )
-//     }
-//   }
-// });
-
-//<LinkButton url={this.props.data.yelpUrl} id={"yelp"} />
-
 
 // Renders the whole application
 React.render(
